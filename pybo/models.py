22848778de35f5f2c2056bb5694091e5d25b69bc
KieraHan <129209383+KieraHan@users.noreply.github.com>
2023-05-04 01:03:35 +0900

    Update models.py
    공지 작성자정보 추가

--- pybo/models.py
+++ pybo/models.py
@@ -62,4 +62,9 @@ class Notice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     slot = db.Column(db.String(20), nullable=False)
+#작성자
+class Writer(db.Model):
+id = db.Column(db.Integer, primary_key=True)
+writer = db.Column(db.Text, nullable=False)
+slot = db.Column(db.String(20), nullable=False)
 
