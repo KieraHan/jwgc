ad3dea76ebbd2e52c28d7401dcab1502856d65ab
KieraHan <129209383+KieraHan@users.noreply.github.com>
2023-09-08 12:42:09 +0900

    Update main_views.py

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -122,15 +122,13 @@ def apply():
         db.session.add(board)
         db.session.commit()
 
-        applicants = ThuBoard.query.filter_by(slot="목1012").all()
+        applicants = FriBoard.query.filter_by(slot="금1012").all()
         names1 = [applicant.user.name for applicant in applicants]
-        applicants = ThuBoard.query.filter_by(slot="목122").all()
+        applicants = FriBoard.query.filter_by(slot="금122").all()
         names2 = [applicant.user.name for applicant in applicants]
-        applicants = ThuBoard.query.filter_by(slot="목24").all()
+        applicants = FriBoard.query.filter_by(slot="금24").all()
         names3 = [applicant.user.name for applicant in applicants]
-        applicants = ThuBoard.query.filter_by(slot="목79").all()
-        names4 = [applicant.user.name for applicant in applicants]
-        return jsonify({"message": "신청이 완료되었습니다.", "names1": names1, "names2": names2, "names3": names3,"names4": names4}), 200
+        return jsonify({"message": "신청이 완료되었습니다.", "names1": names1, "names2": names2, "names3": names3,}), 200
 
     elif day == '토':
         existing_applicant = SatBoard.query.filter_by(slot=slot_name, user_id=user.id).first()  # 수정된 부분
